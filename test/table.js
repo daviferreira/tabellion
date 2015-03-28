b0c1fe3900fafc4da046eef887e850f90bf03526
Davi Ferreira <contato@daviferreira.com>
2015-03-28 22:14:50 +0100

    Rename test file and add delete table test

--- test/table.js
+++ test/table.js
@@ -1,14 +1,24 @@
+// TODO: es6
+
 var jsdom = require('mocha-jsdom');
 var expect = require('unexpected');
 
 var Tabellion = require('../dist/tabellion').Table;
 
-describe('tabellion tests', function () {
+describe('table tests', function () {
   jsdom();
 
   it('should set table element as root', function () {
     var table = document.createElement('table');
     var tabellion = new Tabellion(table);
     expect(tabellion.root, 'to be', table);
+  });
+
+  it('should delete a table', function () {
+    var table = document.createElement('table');
+    document.body.appendChild(table);
+    var tabellion = new Tabellion(table);
+    tabellion.delete();
+    expect(document.body.querySelector('table'), 'to be null');
   });
 });
