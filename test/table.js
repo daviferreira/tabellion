e1212fb8292d18b965cf3a8d65785c28cdfad85c
Davi Ferreira <contato@daviferreira.com>
2015-03-28 22:22:49 +0100

    Better naming

--- test/table.js
+++ test/table.js
@@ -6,16 +6,16 @@ describe('table tests', function () {
   jsdom();
 
   it('should set table element as root', () => {
-    var table = document.createElement('table');
-    var tabellion = new Table(table);
-    expect(tabellion.root, 'to be', table);
+    var tableEl = document.createElement('table');
+    var table = new Table(tableEl);
+    expect(table.root, 'to be', tableEl);
   });
 
   it('should delete a table', () => {
-    var table = document.createElement('table');
-    var tabellion = new Table(table);
-    document.body.appendChild(table);
-    tabellion.delete();
+    var tableEl = document.createElement('table');
+    var table = new Table(tableEl);
+    document.body.appendChild(tableEl);
+    table.delete();
     expect(document.body.querySelector('table'), 'to be null');
   });
 });
