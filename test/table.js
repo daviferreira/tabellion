5d3784ac45a2a0d9864b8d2f1d37735700b1f6fc
Davi Ferreira <contato@daviferreira.com>
2015-03-28 22:26:30 +0100

    Add selector support

--- test/table.js
+++ test/table.js
@@ -5,16 +5,24 @@ import { Table } from '../lib/table';
 describe('table tests', function () {
   jsdom();
 
-  it('should set table element as root', () => {
+  it('should accept a DOM element', () => {
     var tableEl = document.createElement('table');
     var table = new Table(tableEl);
     expect(table.root, 'to be', tableEl);
   });
 
+  it('shoud accept a string selector', () => {
+    var tableEl = document.createElement('table');
+    document.body.appendChild(tableEl);
+    var table = new Table('table');
+    expect(table.root, 'to be', tableEl);
+    table.delete();
+  });
+
   it('should delete a table', () => {
     var tableEl = document.createElement('table');
-    var table = new Table(tableEl);
     document.body.appendChild(tableEl);
+    var table = new Table(tableEl);
     table.delete();
     expect(document.body.querySelector('table'), 'to be null');
   });
